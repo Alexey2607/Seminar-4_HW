a150ee00a45d869b24deec7a8e02d7c636c3e5ed
Alexey2607 <120499593+Alexey2607@users.noreply.github.com>
2022-12-24 09:43:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int result = 1;
+
+// for (int i = 0; i < B; i++)
+// {
+//     result = result*A;
+// }
+
+// Console.WriteLine(result);
+
+
+
+// _______________________________________________________________________
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+// Console.WriteLine("Введите число N");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int Met()
+// {
+//     int sum = 0;
+//     while (N>0)
+
+//     {
+//         int temp = N%10;
+//     sum = sum+temp;
+//     N=N/10;
+
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Сумма всех цифр равна:     " + Met());
+
+// ____________________________________________________________________________
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int [] GetArray()
+
+// {int [] array = new int [8];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.WriteLine("Введите элемент массива");
+// array [i] = Convert.ToInt32(Console.ReadLine());
+
+// }
+// return array;
+// }
+
+// Console.WriteLine(String.Join(";", GetArray()));
+
